5b4d6f129a1e1fb4319d99fd04a10cee1c5c7cc9
Eldar Kashaev <eldar_kashaev@mail.ru>
2019-06-25 23:16:36 +0300

    initial commit

---
+++ src/app/roadmap/roadmap.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-roadmap',
+  templateUrl: './roadmap.component.html',
+  styleUrls: ['./roadmap.component.css']
+})
+export class RoadmapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
